9905d37357a972e2e45d9d6c9abba2b967e82c32
lil climate <lilclimate@gmail.com>
2022-10-29 21:18:14 +0800

    feat: effect捕捉事件，触发事件

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,43 @@
+class ReactiveEffect {
+	private _fn: any; 
+	constructor(_fn) { 
+		this._fn = _fn;
+	}
+
+	run() { 
+		activeEffect = this;
+		this._fn();
+	}
+}
+
+const targetMap = new Map();
+export function track(target, key) { 
+	let depsMap = targetMap.get(target);
+	if (!depsMap) { 
+		depsMap = new Map();
+		targetMap.set(target, depsMap);
+	} 
+
+	let dep = depsMap.get(key);
+	if (!dep) { 
+		dep = new Set();
+		depsMap.set(key, dep);
+	}
+	dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+	const depsMap = targetMap.get(target);	
+	let dep = depsMap.get(key);
+
+	for (const effect of dep) { 
+		effect.run();
+	}
+}
+
+let activeEffect;
+export function effect(fn) { 
+	const _effect = new ReactiveEffect(fn);
+
+	_effect.run();
+}	
