6428d5663192eacfe3773781e8d584e5fb34aaee
lil climate <lilclimate@gmail.com>
2022-10-30 00:11:03 +0800

    feat: 实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -16,3 +16,15 @@ export function reactive(raw) {
 		},
 	});
 }
+
+export function readonly(raw) { 
+	return new Proxy(raw, {
+		get(target, key) {
+			const res = Reflect.get(target, key);
+			return res;
+		},
+		set() {
+			return true	
+		},
+	});
+}
