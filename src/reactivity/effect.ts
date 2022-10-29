8412348c6ed577ab1289a0825c3a26066c0c7e90
lil climate <lilclimate@gmail.com>
2022-10-29 22:31:33 +0800

    feat: 实现effect的scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect {
 	private _fn: any; 
-	constructor(_fn) { 
+	constructor(_fn, public scheduler) { 
 		this._fn = _fn;
 	}
 
@@ -31,13 +31,17 @@ export function trigger(target, key) {
 	let dep = depsMap.get(key);
 
 	for (const effect of dep) { 
-		effect.run();
+		if (effect.scheduler) {
+			effect.scheduler()
+		} else {
+			effect.run();
+		}
 	}
 }
 
 let activeEffect;
-export function effect(fn) { 
-	const _effect = new ReactiveEffect(fn);
+export function effect(fn, options: any = {}) { 
+	const _effect = new ReactiveEffect(fn, options.scheduler);
 
 	_effect.run();
 	return _effect.run.bind(_effect);
